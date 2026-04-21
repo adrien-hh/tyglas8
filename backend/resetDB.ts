c73bdef4c638bb6f81ca032dc059c4566a626c0b
Adrien Huhn <adrien.huhn@gmail.com>
2026-04-21 22:13:30 +0200

    Setting up database

--- backend/resetDB.ts
+++ backend/resetDB.ts
@@ -2,22 +2,29 @@ import "dotenv/config";
 import mongoose from "mongoose";
 import { RewardRepository } from "./repositories/RewardRepository";
 
+// Samedi
 const REWARDS_DATA = [
-  { symbol: "biere", quantity: 40, weight: 16 },
-  { symbol: "cafe", quantity: 500, weight: 24 },
-  {
-    symbol: "volant",
-    quantity: 6,
-    weight: 2.5,
-  },
-  { symbol: "crepe", quantity: 500, weight: 24 },
-  {
-    symbol: "buvette",
-    quantity: 1,
-    weight: 0.5,
-  },
-  { symbol: "perdu", quantity: 10000, weight: 33 },
+  { symbol: "biere", quantity: 50, weight: 26.5 },
+  { symbol: "crepe", quantity: 5, weight: 2.65 },
+  { symbol: "cafe", quantity: 15, weight: 8 },
+  { symbol: "volant", quantity: 3, weight: 1.6 },
+  { symbol: "figurine", quantity: 5, weight: 2.65 },
+  { symbol: "banane", quantity: 6, weight: 3.2 },
+  { symbol: "perdu", quantity: 1000, weight: 55.4 },
 ];
+
+// Dimanche
+/*
+const REWARDS_DATA = [
+  { symbol: "biere", quantity: 49, weight: 26.65 },
+  { symbol: "crepe", quantity: 5, weight: 2.75 },
+  { symbol: "cafe", quantity: 15, weight: 8.2 },
+  { symbol: "volant", quantity: 3, weight: 1.65 },
+  { symbol: "figurine", quantity: 5, weight: 2.75 },
+  { symbol: "banane", quantity: 6, weight: 3.3 },
+  { symbol: "perdu", quantity: 1000, weight: 54.7 },
+];
+*/
 
 async function resetDatabase() {
   try {
